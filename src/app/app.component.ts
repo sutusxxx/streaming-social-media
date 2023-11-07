f27167d43ce14cf2c765797f92468a6f5b60c9b7
sutuslaci <laszlo.sutus@minero.hu>
2023-11-07 14:06:22 +0100

    Add navigation event listener.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { takeUntil } from 'rxjs';
+
+import { Component, OnInit } from '@angular/core';
+import { NavigationStart, Router } from '@angular/router';
+import { BaseComponent } from '@components/base/base.component';
 
 @Component({
 	selector: 'app-root',
 	templateUrl: './app.component.html',
 	styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-	constructor() { }
+export class AppComponent extends BaseComponent implements OnInit {
+	constructor(
+		private readonly router: Router
+	) {
+		super();
+	}
+
+	ngOnInit(): void {
+		this.router.events.pipe(takeUntil(this._unsubscribeAll)).subscribe(event => {
+			if (event instanceof NavigationStart) {
+				const browserRefresh = !this.router.navigated;
+				if (browserRefresh) {
+					console.log(this.router.getCurrentNavigation);
+				}
+			}
+		})
+	}
 }
